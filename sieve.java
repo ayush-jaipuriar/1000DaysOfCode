e7d8e04844f0295ea301a6923da319977b776852
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2020-09-13 22:37:49 +0530

    Create sieve.java
    Printing all prime numbers upto n using Sieve Algorithm

---
+++ sieve.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sieve {
+    static void displayPrimes(int n) {
+        boolean prime[] = new boolean[n+1];
+        for(int i=0;i<n;i++)
+            prime[i] = true;
+
+        for(int p = 2; p*p <=n; p++)
+        {
+            if(prime[p] == true)
+            {
+                for(int i = p*p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+        for(int i = 2; i <= n; i++)
+        {
+            if(prime[i] == true)
+                System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        displayPrimes(n);
+    }
+}
