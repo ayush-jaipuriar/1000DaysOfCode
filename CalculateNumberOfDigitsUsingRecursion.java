ed377dd9022ac7bb1e031c4b62027bd2a04973af
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2020-11-09 23:06:26 +0530

    Update CalculateNumberOfDigitsUsingRecursion.java

--- CalculateNumberOfDigitsUsingRecursion.java
+++ CalculateNumberOfDigitsUsingRecursion.java
@@ -3,9 +3,11 @@ public class Main {
        if (n==0) {
            return 0;
        }
+      
        int smallerAns = calculateDigit(n/10);
        return smallerAns+1;
    }
+   
     public static void main(String args[]) {
         System.out.println(calculateDigit(59872));
     }
