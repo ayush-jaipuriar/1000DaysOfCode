0e8ff262fa26d3780073e02ea220befadc839494
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2022-01-05 23:04:10 +0530

    Implement optimized Bubble Sort
    Signed-off-by: Ayush Jaipuriar <jaipuriar.ayush@gmail.com>

--- AlgoExpert/BubbleSort.java
+++ AlgoExpert/BubbleSort.java
@@ -2,18 +2,25 @@ import java.util.Arrays;
 
 public class BubbleSort {
 	
-	// Time Complexity : O(n^2) 
+	// Time Complexity : O(n^2) , O(n) if array is already sorted thanks to optimization
 	// Space Complexity : O(1)
 
 	public static int[] bubbleSort(int[] array) {
 		int n = array.length;
+		boolean performedSort = false;
 		for (int i = 0; i < n; i++) {
+			performedSort = false;
 			for (int j = 0; j < n - i - 1; j++) {
 				if (array[j] > array[j + 1]) {
 					int temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
+					performedSort = true;
 				}
+			}
+			// No need to perform swaps if no two elements got swapped i.e array is already sorted
+			if (!performedSort) {
+				break;
 			}
 		}
 
