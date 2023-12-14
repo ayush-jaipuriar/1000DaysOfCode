8de6a9b632a41733966a0fc526777b11f5f33b56
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2023-12-14 23:57:24 +0530

    Signed-off-by: Ayush Jaipuriar <jaipuriar.ayush@gmail.com>

--- AlgoExpert/ProductSumRecursion.java
+++ AlgoExpert/ProductSumRecursion.java
@@ -1,14 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductSumRecursion {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int productSum(List<Object> array) {
+	    // Write your code here.
+	    return productSumHelper(array,1);
+	  }
 
-	}
-	
-	public static int productSum(List<Object> inputList) {
-		return -1; 
-	}
+	  public static int productSumHelper(List<Object> array, int multiplier) {
+	    int sum =0;
+	    for (Object obj : array) {
+	      if (obj instanceof ArrayList) {
+	        @SuppressWarnings("unchecked")
+	        ArrayList<Object> ls = (ArrayList<Object>) obj;
+	        sum += productSumHelper(ls, multiplier+1);
+	      } else {
+	        sum += (int) obj;
+	      }
+	    }
+	    return sum * multiplier;
+	  }
 
 }
