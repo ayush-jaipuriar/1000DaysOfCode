8d7740e580647db7fe1f7cc42eb9db15b424f61e
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-02-16 14:26:11 +0530

    [73. Set MatrixZeroes](https://leetcode.com/problems/set-matrix-zeroes/description/)

---
+++ com/striver/sdesheet/SetMatrixZero.java
@@ -0,0 +1,48 @@
+package com.striver.sdesheet;
+
+import java.util.Arrays;
+
+public class SetMatrixZero {
+
+	public static void main(String[] args) {
+
+	}
+	
+	
+	// Better but not the best approach
+	// Uses two seperate arrays to keep track of rows or columns containing zeroes
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] zeroRows = new boolean[m];
+        boolean[] zeroColumns = new boolean[n];
+        // Filling the zeroRows and zeroColumns array as false initially
+        Arrays.fill(zeroRows, false);
+        Arrays.fill(zeroColumns, false);
+    
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n;j++) {
+                // If the matrix[i][j]th element is 0 then we set its corresponding ith and jth index 
+                // in zeroRows and zeroColumns array as true, which would mean that that particular
+                // row or column has a zero
+                if (matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroColumns[j] = true;
+                }
+            }
+        }
+
+        // Now that we have filled the zeroRows and zeroColumns array, we will now iterate through the matrix
+        // again and if the ith or jth index of zeroRows/zeroColumns array is true, we will make that matrix[i][j]th 
+        // element as 0
+
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (zeroRows[i] == true || zeroColumns[j] == true) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }        
+    }
+
+}
