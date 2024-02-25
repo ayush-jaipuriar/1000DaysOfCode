dc2f13e9cb8b175adfaa4ff7f9848cfb2e4c791b
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-02-25 23:04:13 +0530

    Exception Handling using Future in Executor Framework

--- JavaTheory/ExecutorFramework.java
+++ JavaTheory/ExecutorFramework.java
@@ -1,7 +1,9 @@
 package JavaTheory;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +18,19 @@ public class ExecutorFramework {
         ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
         scheduledExecutor.schedule(() -> System.out.println("Running a task which will execute after 5 seconds"), 5, TimeUnit.SECONDS);
         scheduledExecutor.shutdown();
+
+
+        // Handling Exceptions using Final
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        Future<Integer> future = executor.submit(()-> 1/0);
+        try {
+            int result = future.get();
+        } catch(Exception e) {
+            System.out.println("Exception occurred : " + e.getCause());
+        } finally {
+            executor.shutdown();
+        }
+
     }
 
 }
