741bfd5cbec564a82100f7113bedb393ac13056e
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-02-25 22:34:29 +0530

    ScheduledExecutor Example

--- JavaTheory/ExecutorFramework.java
+++ JavaTheory/ExecutorFramework.java
@@ -2,6 +2,8 @@ package JavaTheory;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorFramework {
 
@@ -9,6 +11,11 @@ public class ExecutorFramework {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         executorService.submit(() -> System.out.println("All is vanity"));
         executorService.shutdown();
+
+
+        ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
+        scheduledExecutor.schedule(() -> System.out.println("Running a task which will execute after 5 seconds"), 5, TimeUnit.SECONDS);
+        scheduledExecutor.shutdown();
     }
 
 }
