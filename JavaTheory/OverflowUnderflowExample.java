1b84f0be6a66beea302e24420d1ce50bc56afd4c
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-02-28 13:57:11 +0530

    Overflow and Underflow Example in Java

--- JavaTheory/OverflowUnderflowExample.java
+++ JavaTheory/OverflowUnderflowExample.java
@@ -1,5 +1,13 @@
 package JavaTheory;
 
+class Student {
+    String name;
+
+    Student(String name) {
+        this.name = name;
+    }
+}
+
 public class OverflowUnderflowExample {
     public static void main(String[] args) {
         int maxValue = Integer.MAX_VALUE; // 2147483647
@@ -12,5 +20,13 @@ public class OverflowUnderflowExample {
         // Underflow: Subtracting 1 from the minimum value
         int underflowResult = minValue - 1;
         System.out.println("Underflow result: " + underflowResult); // 2147483647
+
+        Student s1 = new Student("John");
+        Student s2 = new Student("Raj");
+        Student s3 = s2;
+        s2 = new Student("Anita");
+        s2 = s1;
+        System.out.println(s3.name);
+
     }
 }
