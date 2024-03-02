23877f5637cb07253d30a5f097c23dec10fbaf67
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-02 15:51:37 +0530

    Early Binding Showcase for Abstract Method Binding

--- JavaTheory/MethodBinding.java
+++ JavaTheory/MethodBinding.java
@@ -1,5 +1,15 @@
 package JavaTheory;
 
+interface Animal {
+    void sound();
+}
+
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("Woof Woof!");
+    }
+}
+
 class Parent {
     void display() {
         System.out.println("Parent class method");
@@ -19,5 +29,10 @@ public class MethodBinding {
         
         obj1.display(); // Early binding - Parent class method
         obj2.display(); // Late binding - Child class method
+
+        // Method Binding Example for abstract methods which get resolved at compile time 
+        // based on the implementation provided by the implementing method
+        Animal dog = new Dog();
+        dog.sound();
     }
 }
