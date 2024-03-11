580c95db6282b4bf219cf6951c650e32cf7e6474
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-11 23:06:13 +0530

    Java Thread implementation example

--- JavaTheory/MyThread.java
+++ JavaTheory/MyThread.java
@@ -1,23 +1,35 @@
-package JavaTheory;
+package JavaTheory; // Package declaration
 
-class MyThread extends Thread{
-	
-	public void run() {
-		for (int i=0; i<10000; i++) {
-			System.out.println("Thread id : " + Thread.currentThread().getId() + " count = " + i);
+// Class MyThread extends the Thread class, allowing it to be used as a thread
+class MyThread extends Thread {
+    
+    // Override the run method from the Thread class
+    // This method contains the code that will be executed when the thread is started
+    @Override
+    public void run() {
+        // Loop 10000 times
+        for (int i = 0; i < 10000; i++) {
+            // Print the thread ID and the current iteration count
+            // Note: Using System.out.println is generally discouraged in production code due to its synchronization overhead. Consider using a logger.
+            System.out.println("Thread id : " + Thread.currentThread().getId() + " count = " + i);
+            
             try {
-                // Introduce a short delay
+                // Introduce a short delay to simulate work being done
                 Thread.sleep(100);
             } catch (InterruptedException e) {
+                // Handle the InterruptedException
+                // Note: It's recommended to either re-interrupt the thread or rethrow the exception
                 e.printStackTrace();
             }
-		}
-	}
-	
-	public static void main(String[] args) {
-		MyThread myThread = new MyThread();
-		myThread.start();
-	}
-
+        }
+    }
+    
+    // Main method to start the execution of the program
+    public static void main(String[] args) {
+        // Create an instance of MyThread
+        MyThread myThread = new MyThread();
+        
+        // Start the thread
+        myThread.start();
+    }
 }
-
