68ad18fe7486bc83155a24e9ae2288a1f3ca8804
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-16 22:21:41 +0530

    Stream anyMatch() allMatch() noneMatch() methods

---
+++ JavaTheory/StreamMatch.java
@@ -0,0 +1,37 @@
+package JavaTheory;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMatch {
+    public static void main(String[] args) {
+        // anyMatch() - Checking for odd numbers
+        List<Integer> numbers = Arrays.asList(2, 4, 6, 7, 8);
+        boolean anyOdd = numbers.stream().anyMatch(num -> num % 2 == 0);
+        System.out.println(anyOdd);
+
+
+        // allMatch - All Strings above a length
+        List<String> words = Arrays.asList("computer", "science", "technology");
+        boolean allLongWords = words.stream().allMatch(word -> word.length() > 5);
+        System.out.println(allLongWords);
+
+        // noneMatch() - none negative numbers
+        numbers = Arrays.asList(3, 5, 8, 10);
+        boolean noneNegative = numbers.stream().noneMatch(num -> num <0);
+        System.out.println(noneNegative);
+
+
+        // anyMatch() - Finding specific file type
+        File directory = new File("C:\\Users\\lambo\\Downloads");
+        File[] files = directory.listFiles();
+
+        boolean containsPdf = Arrays.stream(files).peek(file -> System.out.println("File Name : " + file.getName())).anyMatch(file -> file.getName().endsWith("rar"));
+        System.out.println("Directory contains PDF file? : " + containsPdf);
+
+
+        
+    }
+}
