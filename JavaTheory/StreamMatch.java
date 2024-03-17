49e1ebe50fc357ad33555b84087725e8d18e18fc
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-03-17 23:26:49 +0530

    Stream Match Example Usecases

--- JavaTheory/StreamMatch.java
+++ JavaTheory/StreamMatch.java
@@ -12,7 +12,6 @@ public class StreamMatch {
         boolean anyOdd = numbers.stream().anyMatch(num -> num % 2 == 0);
         System.out.println(anyOdd);
 
-
         // allMatch - All Strings above a length
         List<String> words = Arrays.asList("computer", "science", "technology");
         boolean allLongWords = words.stream().allMatch(word -> word.length() > 5);
@@ -20,18 +19,16 @@ public class StreamMatch {
 
         // noneMatch() - none negative numbers
         numbers = Arrays.asList(3, 5, 8, 10);
-        boolean noneNegative = numbers.stream().noneMatch(num -> num <0);
+        boolean noneNegative = numbers.stream().noneMatch(num -> num < 0);
         System.out.println(noneNegative);
-
 
         // anyMatch() - Finding specific file type
         File directory = new File("C:\\Users\\lambo\\Downloads");
         File[] files = directory.listFiles();
 
-        boolean containsPdf = Arrays.stream(files).peek(file -> System.out.println("File Name : " + file.getName())).anyMatch(file -> file.getName().endsWith("rar"));
+        boolean containsPdf = Arrays.stream(files).peek(file -> System.out.println("File Name : " + file.getName()))
+                .anyMatch(file -> file.getName().endsWith("rar"));
         System.out.println("Directory contains PDF file? : " + containsPdf);
 
-
-        
     }
 }
