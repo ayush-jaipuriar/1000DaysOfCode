367493ff80b96e4247dd8e7b7ee88a610498e71f
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-04-22 14:27:01 +0530

    Stream : Numeric Streams and Terminal min(), max() and average() operations, add more examples

--- JavaTheory/StreamNumericStreams.java
+++ JavaTheory/StreamNumericStreams.java
@@ -11,50 +11,55 @@ public class StreamNumericStreams {
     public static void main(String[] args) {
         // IntStream range()
         System.out.println("Range :");
-        IntStream intStream = IntStream.range(1,10);
+        IntStream intStream = IntStream.range(1, 10);
         intStream.forEach(System.out::println);
 
         // IntStream rangeClosed()
         System.out.println("Range Closed :");
-        intStream = IntStream.rangeClosed(1,10);
+        intStream = IntStream.rangeClosed(1, 10);
         intStream.forEach(System.out::println);
 
         // DoubleStream
         System.out.println("Double Stream : ");
-        DoubleStream doubleStream = IntStream.range(1,10).asDoubleStream();
+        DoubleStream doubleStream = IntStream.range(1, 10).asDoubleStream();
         doubleStream.forEach(System.out::println);
 
-        // You're tasked with calculating the sum of squares of even numbers within a range. How would you implement this using numeric streams??
+        // You're tasked with calculating the sum of squares of even numbers within a
+        // range. How would you implement this using numeric streams??
         System.out.println("Sum of even numbers : ");
-        int sumOfEvenSquares = IntStream.rangeClosed(1,10).filter(num -> num%2==0).map(num -> num * 2).sum();
+        int sumOfEvenSquares = IntStream.rangeClosed(1, 10).filter(num -> num % 2 == 0).map(num -> num * 2).sum();
         System.out.println(sumOfEvenSquares);
 
         // Basic Sum Calcualtion
-        int sum = IntStream.of(1,2,3,4,5).sum();
+        int sum = IntStream.of(1, 2, 3, 4, 5).sum();
         System.out.println(sum);
 
         // Finding Maximum
-        OptionalInt  max = IntStream.rangeClosed(1,10).max();
+        OptionalInt max = IntStream.rangeClosed(1, 10).max();
         System.out.println(max);
 
         // Finding minimum
-        OptionalInt min = IntStream.rangeClosed(1,10).min();
+        OptionalInt min = IntStream.rangeClosed(1, 10).min();
         System.out.println(min);
 
         // Average Calculation
-        OptionalDouble  average = DoubleStream.of(1,2,3,4,5).average();
+        OptionalDouble average = DoubleStream.of(1, 2, 3, 4, 5).average();
         System.out.println(average);
 
         // Boxing with Numeric Streams
-        List<Integer> boxedIntegerList = IntStream.rangeClosed(1,10).boxed().collect(Collectors.toList());
+        List<Integer> boxedIntegerList = IntStream.rangeClosed(1, 10).boxed().collect(Collectors.toList());
         System.out.println(boxedIntegerList);
 
         // Unboxing with Numeric Streams
         sum = boxedIntegerList.stream().mapToInt(Integer::intValue).sum();
         System.out.println(sum);
 
-        //
-        
+        List<String> words = List.of("Java", "Stream", "API");
+        int sumOfLengths = words.stream().mapToInt(String::length).sum();        
+        System.out.println("Sum of lengths of strings: " + sumOfLengths);
+
+        OptionalDouble averageOfLengths = words.stream().mapToInt(String::length).average();
+        System.out.println("Average of lengths of strings : " + averageOfLengths.getAsDouble());
     }
 
 }
