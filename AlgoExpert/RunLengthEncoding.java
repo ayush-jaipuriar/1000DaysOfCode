823733dde7342f1469ac7cb6da4aac8b82617f70
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-05-31 23:05:24 +0530

    AlgoExpert : Run Length Encoding

--- AlgoExpert/RunLengthEncoding.java
+++ AlgoExpert/RunLengthEncoding.java
@@ -1,33 +1,35 @@
+public static String runLengthEncoding(String string) {
+	int count = 0; // Initialize count to keep track of consecutive characters
+	StringBuilder sb = new StringBuilder(); // Use StringBuilder for efficient string concatenation
+	Character lastConsecutiveCharacter = string.charAt(0); // Start with the first character of the input string
 
-public class RunLengthEncoding {
-	
-	// Time Complexity : O(n) where n is the length of the string
-	// Space Complexity : O(n) where n is the length of the string
-	
-	
-	public static String runLengthEncoding(String string) {
-		int count = 0;
-		StringBuilder sb = new StringBuilder();
-		Character lastConsecutiveCharacter = string.charAt(0);
-		for (int i = 0; i <= string.length(); i++) {
-			if (i == string.length()) {
-				sb.append(String.valueOf(count) + lastConsecutiveCharacter);
-				break;
-			}
-			char st = string.charAt(i);
-			if (st == lastConsecutiveCharacter && count < 9) {
-				count++;
-			} else {
-				sb.append(String.valueOf(count) + lastConsecutiveCharacter);
-				count = 1;
-			}
-			lastConsecutiveCharacter = st;
+	// Iterate through each character in the string
+	for (int i = 0; i <= string.length(); i++) {
+		// Check if we've reached the end of the string
+		if (i == string.length()) {
+			sb.append(String.valueOf(count) + lastConsecutiveCharacter); // Append the count and character before
+																			// breaking out of the loop
+			break;
 		}
-		return sb.toString();
+		char st = string.charAt(i); // Get the current character
+
+		// If the current character is the same as the last consecutive character and
+		// count is less than 9,
+		// increment the count
+		if (st == lastConsecutiveCharacter && count < 9) {
+			count++;
+		} else {
+			// Otherwise, append the count and the last consecutive character to the
+			// StringBuilder,
+			// then reset the count to 1
+			sb.append(String.valueOf(count) + lastConsecutiveCharacter);
+			count = 1;
+		}
+
+		// Update the lastConsecutiveCharacter to the current character for the next
+		// iteration
+		lastConsecutiveCharacter = st;
 	}
 
-	public static void main(String[] args) {
-		runLengthEncoding("AAAAAAAAAAAAABBCCCCDD");
-	}
-
+	return sb.toString(); // Return the encoded string
 }
