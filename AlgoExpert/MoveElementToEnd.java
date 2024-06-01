e53d69cf63a97e9cce5f20d9b23e8326db2afa06
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-06-01 22:21:42 +0530

    AlgoExpert : Move Elements to the end

--- AlgoExpert/MoveElementToEnd.java
+++ AlgoExpert/MoveElementToEnd.java
@@ -1,12 +1,20 @@
 import java.util.List;
 
 public class MoveElementToEnd {
+    /**
+     * Main method placeholder. Intended for testing purposes.
+     */
     public static void main(String[] args) {
 
     }
 
-    // Time Complexity : O(n)
-    // Space Complexity : O(1)
+    /**
+     * Moves all occurrences of a specified element to the end of a list.
+     * 
+     * @param array The input list containing elements.
+     * @param toMove The integer value to move to the end of the list.
+     * @return A list with all occurrences of {@code toMove} moved to the end.
+     */
     public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
         int n = array.size();
         int start = 0;
@@ -23,10 +31,16 @@ public class MoveElementToEnd {
         return array;
     }
 
+    /**
+     * Swaps two elements in the list.
+     * 
+     * @param i Index of the first element to swap.
+     * @param j Index of the second element to swap.
+     * @param array The list in which to perform the swap.
+     */
     public static void swap(int i, int j, List<Integer> array) {
         int temp = array.get(j);
         array.set(j, array.get(i));
         array.set(i, temp);
     }
-
 }
