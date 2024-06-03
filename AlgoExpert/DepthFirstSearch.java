77720d1c392816d04d1b1aaeb6973cbfb6ed7cfc
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-06-03 22:11:36 +0530

    AlgoExpert : Depth First Search

--- AlgoExpert/DepthFirstSearch.java
+++ AlgoExpert/DepthFirstSearch.java
@@ -14,13 +14,20 @@ class DFS {
             return this;
         }
 
+        // Performs a depth-first search starting from the current node,
+        // adding the node's name to the provided list and recursively doing the same for all its children.
         List<String> depthFirstSearch(List<String> array) {
+            // Add the current node's name to the array
             array.add(this.name);
+
+            // Iterate over each child of the current node
             for (int i=0; i<children.size(); i++) {
+                // Recursively perform depth-first search on the child node
                 children.get(i).depthFirstSearch(array);
             }
+
+            // Return the array containing the names of all visited nodes in the order they were visited
             return array;
         }
-
     }
 }
