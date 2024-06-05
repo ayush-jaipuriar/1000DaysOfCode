2bfa66e452dc52cd650512da81f09541a6a1818a
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-06-05 23:08:01 +0530

    Insertion Sort Revisited

--- AlgoExpert/InsertionSort.java
+++ AlgoExpert/InsertionSort.java
@@ -1,27 +1,39 @@
 import java.util.Arrays;
 
+/**
+ * Class to demonstrate the insertion sort algorithm.
+ */
 public class InsertionSort {
 	
-	// Time Complexity : O(n^2)
-	// Space Complexity : O(1)
-
-	public static int[] performInsertionSort(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i; j > 0; j--) {
-				if (arr[j] < arr[j - 1]) {
-					int temp = arr[j];
-					arr[j] = arr[j - 1];
-					arr[j - 1] = temp;
-				}
-			}
-		}
-		return arr;
-	}
-
-	public static void main(String[] args) {
-		int[] unsortedArray = { 3, 2, 1,-23,0, 52 };
-		int[] sortedArray = performInsertionSort(unsortedArray);
-		System.out.println(Arrays.toString(unsortedArray));
-	}
-
+    // Time Complexity : O(n^2)
+    // Space Complexity : O(1)
+    
+    /**
+     * Performs the insertion sort on the given array.
+     * @param arr The array to be sorted.
+     * @return The sorted array.
+     */
+    public static int[] performInsertionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+        return arr;
+    } 
+	
+    /**
+     * Main method to test the insertion sort algorithm.
+     */
+    public static void main(String[] args) {
+        int[] unsortedArray = { 3, 2, 1,-23,0, 52 };
+        int[] sortedArray = performInsertionSort(unsortedArray);
+        // Removed the print statement that prints the unsorted array instead of the sorted one.
+        // Uncomment the next line to see the sorted array.
+        // System.out.println(Arrays.toString(sortedArray));
+    }
 }
