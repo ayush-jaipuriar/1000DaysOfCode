bfbd51f8089e7eab62a3fc660164de4f1a87009f
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-06-08 23:48:15 +0530

    JS Strings : parseInt() and parseFloat() functions

--- JavaScript/String.js
+++ JavaScript/String.js
@@ -36,3 +36,30 @@ console.log(text.substring(-5, -1));
 // 2. Index Order : If the start index is greater than the end index in slice(), it returns an empty string. In substring(), it swaps the indices.
 console.log(text.slice(4, 2)); // empty string ""
 console.log(text.substring(4,2)); // am
+
+// Converting a String to a number
+let str1 = "42";
+let num1 = parseInt(str1);
+console.log(typeof str1);
+console.log(typeof num1);
+console.log(num1);
+
+let str2 = "3.14";
+let num2 = parseFloat(str2);
+console.log(typeof str2);
+console.log(typeof num2);
+console.log(num2);
+
+let str3 = "123";
+let num3 = +str3;
+console.log(typeof str3);
+console.log(typeof num3);
+console.log(num3);
+
+// FU : Trying to parse a String that is not a number will return NaN
+let str4 = "Ayush";
+let num4 = parseInt(str4);
+console.log(num4); // NaN
+str4 = "Ayush123";
+console.log(parseInt(str4)); // NaN
+
