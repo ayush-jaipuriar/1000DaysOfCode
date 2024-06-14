c7bb6c698fbe81e2cebd78b092e38dac930bfb09
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-06-14 23:22:28 +0530

    AlgoExpert : Depth First Search

--- AlgoExpert/DepthFirstSearch.java
+++ AlgoExpert/DepthFirstSearch.java
@@ -1,27 +1,32 @@
 import java.util.*;
-class DFS {
-    static class Node {
-        String name;
-        List<Node> children = new ArrayList<>();
 
-        Node (String name) {
+// Define the main class for performing depth-first searches
+class DFS {
+    // Inner class representing a node in the graph/tree
+    static class Node {
+        String name; // Name of the node
+        List<Node> children = new ArrayList<>(); // Children of the node
+
+        // Constructor for creating a new node with a given name
+        Node(String name) {
             this.name = name;
         }
 
+        // Method to add a child node to the current node
         Node addChild(String name) {
-            Node child = new Node(name);
-            children.add(child);
-            return this;
+            Node child = new Node(name); // Create a new child node
+            children.add(child); // Add the child to the current node's children
+            return this; // Return the current node for method chaining
         }
 
-        // Performs a depth-first search starting from the current node,
-        // adding the node's name to the provided list and recursively doing the same for all its children.
+        // Method to perform a depth-first search starting from the current node
+        // It adds the node's name to the provided list and recursively does the same for all its children
         List<String> depthFirstSearch(List<String> array) {
             // Add the current node's name to the array
             array.add(this.name);
 
             // Iterate over each child of the current node
-            for (int i=0; i<children.size(); i++) {
+            for (int i = 0; i < children.size(); i++) {
                 // Recursively perform depth-first search on the child node
                 children.get(i).depthFirstSearch(array);
             }
@@ -30,4 +35,4 @@ class DFS {
             return array;
         }
     }
-}
+}s
