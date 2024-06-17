ca13a24ee7ac891d1743dc363e264ebd8da0842b
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-06-17 23:31:22 +0530

    AlgoExpert : Linked List Middle Node

--- AlgoExpert/LinkedListMiddleNode.java
+++ AlgoExpert/LinkedListMiddleNode.java
@@ -3,27 +3,28 @@ import java.util.*;
 class Program {
     // This is an input class. Do not edit.
     public static class LinkedList {
-        public int value;
-        public LinkedList next;
+        public int value; // Node value
+        public LinkedList next; // Reference to the next node
 
         public LinkedList(int value) {
-            this.value = value;
-            this.next = null;
+            this.value = value; // Initialize node with a value
+            this.next = null; // Next node is initially null
         }
     }
 
+    // Method to find the middle node of a linked list
     public LinkedList middleNode(LinkedList linkedList) {
-        // Initialize two pointers: slowPointer and fastPointer
-        LinkedList slowPointer = linkedList;
-        LinkedList fastPointer = linkedList;
+        // Initialize two pointers: slowPointer and fastPointer at the head of the list
+        LinkedList slowPointer = linkedList; // Moves one step at a time
+        LinkedList fastPointer = linkedList; // Moves two steps at a time
 
-        // Move fastPointer twice as fast as slowPointer
+        // Loop until fastPointer reaches the end of the list
         while (fastPointer != null && fastPointer.next != null) {
-            slowPointer = slowPointer.next;
-            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next; // Move slowPointer one step
+            fastPointer = fastPointer.next.next; // Move fastPointer two steps
         }
 
-        // At this point, slowPointer points to the middle node
+        // At this point, slowPointer points to the middle node of the list
         return slowPointer;
     }
 }
