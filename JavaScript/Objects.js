fa4a0d46ec27845616ea1b717c68a0e64d1d14c4
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-07-22 23:19:09 +0530

    feat: Add Person constructor and greet method to Objects.js

---
+++ JavaScript/Objects.js
@@ -0,0 +1,20 @@
+// Function Constructors
+
+// Define a function constructor called Person
+function Person(firstname, lastname) {
+    // Inside the constructor, set the firstname and lastname properties of the object being created
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
+
+// Add a greet method to the prototype of the Person constructor
+Person.prototype.greet = function() {
+    // Inside the greet method, log a greeting message using the firstname and lastname properties
+    console.log(`Hello, ${this.firstname} ${this.lastname}`);
+}
+
+// Create a new instance of the Person object using the 'new' keyword and pass in the firstname and lastname values
+let ayush = new Person('Ayush', 'Jaipuriar');
+
+// Call the greet method on the ayush object
+ayush.greet();
