0f099fd1281fa640f6429adf6b7ba800fd5b3da8
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-09-01 23:34:10 +0530

    Added comments

--- TUF/SumOfFirstNNumbers.java
+++ TUF/SumOfFirstNNumbers.java
@@ -10,9 +10,11 @@ class Solution {
     // Time Complexity: O(N)
     // Space Complexity: O(1)
     public int nNumberSumHelper(int n) {
+        // Base case: If n is 1, return 1 because we need to sum 1 number
         if (n == 1) {
             return 1;
         }
+        
         return n + nNumberSumHelper(n-1);
     }
 }
