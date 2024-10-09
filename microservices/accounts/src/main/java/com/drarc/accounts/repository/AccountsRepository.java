def3d401846e6654b0a101d238eb01781513a2d7
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-10-09 20:52:10 +0530

    Added skeleton repository CustomerRepository and AccountsRepository classes

---
+++ microservices/accounts/src/main/java/com/drarc/accounts/repository/AccountsRepository.java
@@ -0,0 +1,9 @@
+package com.drarc.accounts.repository;
+
+import com.drarc.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+}
---
+++ microservices/accounts/src/main/java/com/drarc/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.drarc.accounts.repository;
+
+import com.drarc.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
