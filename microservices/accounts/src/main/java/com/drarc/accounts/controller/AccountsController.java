afa5216d4a0c328d2293853df2e7b47aa07db35a
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-10-10 21:18:47 +0530

    Tie the AccountsService to the AccountsController

--- microservices/accounts/src/main/java/com/drarc/accounts/controller/AccountsController.java
+++ microservices/accounts/src/main/java/com/drarc/accounts/controller/AccountsController.java
@@ -3,6 +3,8 @@ package com.drarc.accounts.controller;
 import com.drarc.accounts.constants.AccountsConstants;
 import com.drarc.accounts.dto.CustomerDto;
 import com.drarc.accounts.dto.ResponseDto;
+import com.drarc.accounts.service.IAccountsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path="/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AccountsController {
 
+    @Autowired
+    private IAccountsService accountsService;
+
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
         // Business logic will be added here later
+        accountsService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.STATUS_MESSAGE_201));
     }
 
