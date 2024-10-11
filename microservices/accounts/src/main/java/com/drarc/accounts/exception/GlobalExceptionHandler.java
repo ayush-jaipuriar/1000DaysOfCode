690f4a775ac1e7c31a24907537252ecd8651e3c3
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-10-11 22:22:10 +0530

    Override handleMethodArgumentNotValid exception to display list of all failed validations as response

--- microservices/accounts/src/main/java/com/drarc/accounts/exception/GlobalExceptionHandler.java
+++ microservices/accounts/src/main/java/com/drarc/accounts/exception/GlobalExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.drarc.accounts.exception;
 
 import com.drarc.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handle(Exception ex, WebRequest request) {
@@ -34,4 +40,19 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>();
+        // Iterate over field errors and populate them into the validationErrors map
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            validationErrors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+
+
 }
