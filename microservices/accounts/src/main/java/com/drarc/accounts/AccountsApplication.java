c2aaf2f6110708c2428385f4f250b23803db465f
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2024-10-12 13:07:22 +0530

    Enhance the Swagger documentation by using @Tag, @Operation and @ApiResponse annotations

--- microservices/accounts/src/main/java/com/drarc/accounts/AccountsApplication.java
+++ microservices/accounts/src/main/java/com/drarc/accounts/AccountsApplication.java
@@ -3,6 +3,11 @@ package com.drarc.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
--- microservices/accounts/src/main/java/com/drarc/accounts/controller/AccountsController.java
+++ microservices/accounts/src/main/java/com/drarc/accounts/controller/AccountsController.java
@@ -4,6 +4,10 @@ import com.drarc.accounts.constants.AccountsConstants;
 import com.drarc.accounts.dto.CustomerDto;
 import com.drarc.accounts.dto.ResponseDto;
 import com.drarc.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +21,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path="/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @Validated
+@Tag(name = "CRUD REST APIs for Accounts", description = "CRUD REST APIs to CREATE and FETCH Bank Account Details")
 public class AccountsController {
 
     @Autowired
     private IAccountsService accountsService;
 
     @PostMapping("/create")
+    @Operation(summary = "Create Account REST API", description = "REST API to create a new Customer and Account in Bank")
+    @ApiResponse(responseCode = "201", description = "HTTPStatus.CREATED")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         accountsService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.STATUS_MESSAGE_201));
     }
 
     @GetMapping("/fetch")
+    @Operation(summary = "Fetch Account REST API", description = "REST API to fetch Customer and Account Details using Mobile Number")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam @Pattern(regexp = "\\d{10}", message = "Mobile number must be 10 digits") String mobileNumber) {
         CustomerDto customerDto = accountsService.fetchAccount(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
